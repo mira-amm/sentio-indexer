b425e5bbcb2648b6e16a2382f3307fed0acf6c94
David Mihal <david@dmihal.com>
2024-09-26 17:28:29 +0100

    Index all transfers

--- src/processor.ts
+++ src/processor.ts
@@ -13,7 +13,7 @@
 
 import { LogLevel, } from '@sentio/sdk'
 import { FuelGlobalProcessor, FuelNetwork, FuelProcessor } from '@sentio/sdk/fuel';
-import { InputType, OutputType, Input, bn, ReceiptType } from 'fuels';
+import { InputType, OutputType, Input, bn, ReceiptType, ZeroBytes32 } from 'fuels';
 import { AmmProcessor } from './types/fuel/AmmProcessor.js'
 import { AssetIdInput } from './types/fuel/Amm.js';
 import crypto from 'crypto';
@@ -36,12 +36,12 @@ function getLPAssetId(poolId: PoolId) {
     .update(Buffer.from(poolId[2] ? '01' : '00', 'hex'))
     .digest();
 
-  return crypto.createHash('sha256').update(contractBuffer).update(subId).digest('hex');
+  return '0x' + crypto.createHash('sha256').update(contractBuffer).update(subId).digest('hex');
 }
 
 const processor = AmmProcessor.bind({
   address: AMM_CONTRACT_ADDRESS,
-  chainId: FuelNetwork.TEST_NET
+  chainId: FuelNetwork.TEST_NET,
 });
 
 processor.onLogCreatePoolEvent(async (event, ctx) => {
@@ -87,3 +87,95 @@ processor.onLogBurnEvent(async (event, ctx) => {
     lpAssetId: event.data.liquidity.id.bits,
   });
 });
+
+
+FuelGlobalProcessor
+  .bind({
+    chainId: FuelNetwork.TEST_NET,
+  })
+  .onTransaction(
+    async (tx, ctx) => {
+      const txDate = tx.date ? normalizeTxDate(tx.date) : null;
+
+      const assetsBalancesDiffs: Record<string, Record<string, bigint>> = {};
+
+      if (tx.status === 'success') {
+        for (const receipt of tx.receipts) {
+          if (receipt.type === ReceiptType.LogData) {
+            // Check for SRC20 metadata events
+          } else if (receipt.type === ReceiptType.Transfer || (receipt.type === ReceiptType.Call && receipt.amount.lt(bn(0)))) {
+            const assetsBaseOwner = assetsBalancesDiffs[receipt.to] ?? {};
+            const assetsBaseOwnerBalance = assetsBaseOwner[receipt.assetId] ?? 0n;
+            assetsBaseOwner[receipt.assetId] = assetsBaseOwnerBalance + BigInt(receipt.amount.toHex());
+            assetsBalancesDiffs[receipt.to] = assetsBaseOwner;
+
+            if (receipt.from !== ZeroBytes32) {
+              const ownerDiff = assetsBalancesDiffs[receipt.from] ?? {};
+              const ownerDiffBalance = ownerDiff[receipt.assetId] ?? 0n;
+
+              ownerDiff[receipt.assetId] = ownerDiffBalance - BigInt(receipt.amount.toHex());
+              assetsBalancesDiffs[receipt.from] = ownerDiff;
+            }
+          }
+        }
+      }
+
+      const outputs = tx.transaction.outputs || [];
+      const inputs = tx.transaction.inputs || [];
+
+      for (const output of outputs) {
+        switch (output.type) {
+          case OutputType.Coin:
+          case OutputType.Change:
+          case OutputType.Variable:
+            const assetsBaseOwner = assetsBalancesDiffs[output.to] ?? {};
+            const assetsBaseOwnerBalance = assetsBaseOwner[output.assetId] ?? 0n;
+            assetsBaseOwner[output.assetId] = assetsBaseOwnerBalance + BigInt(output.amount.toHex());
+            assetsBalancesDiffs[output.to] = assetsBaseOwner;
+            break;
+        }
+      }
+      for (const input of inputs) {
+        switch (input.type) {
+          case InputType.Coin:
+            const ownerDiff = assetsBalancesDiffs[input.owner] ?? {};
+            const ownerDiffBalance = ownerDiff[input.assetId] ?? 0n;
+
+            ownerDiff[input.assetId] = ownerDiffBalance - BigInt(input.amount.toHex());
+            assetsBalancesDiffs[input.owner] = ownerDiff;
+            break;
+          case InputType.Message:
+            const ownerDiffM = assetsBalancesDiffs[input.recipient] ?? {};
+            const ownerDiffBalanceM = ownerDiffM[BASE_ASSET_ID] ?? 0n;
+
+            ownerDiffM[BASE_ASSET_ID] = ownerDiffBalanceM - BigInt(input.amount.toHex());
+            assetsBalancesDiffs[input.recipient] = ownerDiffM;
+
+            // If message coin has never been seen before log as a balance change positive
+            const amount = input.amount.toString();
+            ctx.eventLogger.emit('assetBalance', {
+              distinctId: input.recipient,
+              txDate,
+              assetId: BASE_ASSET_ID,
+              amount,
+            });
+
+            break;
+        }
+      }
+
+      // Emit all asset balance diffs
+      Object.entries(assetsBalancesDiffs).forEach(([owner, assetsBalances]) => {
+        Object.entries(assetsBalances).forEach(([assetId, assetBalanceDiff]) => {
+          if (assetBalanceDiff === 0n) {
+            ctx.eventLogger.emit('assetBalance', {
+              distinctId: owner,
+              txDate,
+              assetId,
+              amount: assetBalanceDiff.toString(),
+            });
+          }
+        });
+      });
+    },
+  );
