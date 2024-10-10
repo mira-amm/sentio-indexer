d678d3cd3648689c73dff11828665f7021585cf1
David Mihal <david@dmihal.com>
2024-10-10 09:03:50 -0600

    Minor updates to pools

--- queries/pools.sql
+++ queries/pools.sql
@@ -1,5 +1,5 @@
 SELECT
-    pc.chain AS chain_id,
+    '9889' AS chain_id,
     pc.block_number AS creation_block_number,
     pc.timestamp AS timestamp,
     pc.lpAssetId AS pool_address,
@@ -9,7 +9,7 @@ SELECT
     COALESCE(va.symbol, 'UNKNOWN') AS token_symbol,  -- Use symbol from VerifiedAsset table or 'UNKNOWN' if not found
     COALESCE(va.decimals, 0) AS token_decimals,  -- Use decimals from VerifiedAsset or default to 0
     0 AS token_index,  -- Index for token0
-    CASE WHEN pc.stable = 0 THEN 0.003 ELSE 0.0 END AS fee_rate,
+    CASE WHEN pc.stable = 0 THEN 0.003 ELSE 0.005 END AS fee_rate,
     'CPMM' AS dex_type  -- Constant DEX type
 FROM PairCreated pc
 LEFT JOIN VerifiedAsset va ON pc.token0 = va.assetId  -- Join with VerifiedAsset for token0
@@ -17,7 +17,7 @@ LEFT JOIN VerifiedAsset va ON pc.token0 = va.assetId  -- Join with VerifiedAsset
 UNION ALL
 
 SELECT
-    pc.chain AS chain_id,
+    '9889' AS chain_id,
     pc.block_number AS creation_block_number,
     pc.timestamp AS timestamp,
     pc.lpAssetId AS pool_address,
@@ -27,7 +27,7 @@ SELECT
     COALESCE(va.symbol, 'UNKNOWN') AS token_symbol,  -- Use symbol from VerifiedAsset table or 'UNKNOWN' if not found
     COALESCE(va.decimals, 0) AS token_decimals,  -- Use decimals from VerifiedAsset or default to 0
     1 AS token_index,  -- Index for token1
-    CASE WHEN pc.stable = 0 THEN 0.03 ELSE 0.005 END AS fee_rate,
+    CASE WHEN pc.stable = 0 THEN 0.003 ELSE 0.005 END AS fee_rate,
     'CPMM' AS dex_type  -- Constant DEX type
 FROM PairCreated pc
 LEFT JOIN VerifiedAsset va ON pc.token1 = va.assetId  -- Join with VerifiedAsset for token1;
