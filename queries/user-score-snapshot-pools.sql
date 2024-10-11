e2df8a2eead74a1bf035d45a51e4a82d748c9f63
David Mihal <david@dmihal.com>
2024-10-11 08:48:21 -0600

    Attach swaps

--- queries/user-score-snapshot-pools.sql
+++ queries/user-score-snapshot-pools.sql
@@ -33,8 +33,8 @@ mint_agg AS (
         toStartOfHour(timestamp) AS hour,
         poolId,
         sum(liquidity) AS total_minted,
-        SUM(token0In) AS token_0_inflow,
-        SUM(token1In) AS token_1_inflow
+        SUM(token0In) AS token_0_in,
+        SUM(token1In) AS token_1_in
     FROM
         Mint
     GROUP BY
@@ -45,38 +45,51 @@ burn_agg AS (
         toStartOfHour(timestamp) AS hour,
         poolId,
         sum(liquidity) AS total_burned,
-        SUM(token0Out) AS token_0_outflow,
-        SUM(token1Out) AS token_1_outflow
+        SUM(token0Out) AS token_0_out,
+        SUM(token1Out) AS token_1_out
     FROM
         Burn
     GROUP BY
         hour, poolId
 ),
+swap_agg AS (
+    SELECT
+        s.poolId,
+        toStartOfHour(s.timestamp) AS hour,
+        SUM(s.token0In) AS token_0_in,
+        SUM(s.token1In) AS token_1_in,
+        SUM(s.token0Out) AS token_0_out,
+        SUM(s.token1Out) AS token_1_out
+    FROM Swap s
+    GROUP BY hour, poolId
+),
 hourly_pool_aggregated AS (
-  SELECT
-      p.hour,
-      p.pool_id AS pool_id,
-      COALESCE(m.total_minted, 0) AS total_minted,
-      COALESCE(b.total_burned, 0) AS total_burned,
-      (COALESCE(m.total_minted, 0) - COALESCE(total_burned, 0)) AS net_minted,
-      COALESCE(m.token_0_inflow, 0) - COALESCE(b.token_0_outflow) AS token_0_inflow,
-      COALESCE(m.token_1_inflow, 0) - COALESCE(b.token_1_outflow) AS token_1_inflow
-  FROM
-      hourly_pools p
-  LEFT JOIN
-      mint_agg m ON p.hour = m.hour AND p.pool_id = m.poolId
-  LEFT JOIN
-      burn_agg b ON p.hour = b.hour AND p.pool_id = b.poolId
-  ORDER BY
-      p.hour, p.pool_id
+    SELECT
+        p.hour,
+        p.pool_id AS pool_id,
+        COALESCE(m.total_minted, 0) AS total_minted,
+        COALESCE(b.total_burned, 0) AS total_burned,
+        (COALESCE(m.total_minted, 0) - COALESCE(total_burned, 0)) AS net_minted,
+        (COALESCE(m.token_0_in, 0) + COALESCE(s.token_0_in) - COALESCE(b.token_0_out) - COALESCE(s.token_0_out)) / POW(10, p.token_0_decimals) AS token_0_in,
+        (COALESCE(m.token_1_in, 0) + COALESCE(s.token_1_in) - COALESCE(b.token_1_out) - COALESCE(s.token_1_out)) / POW(10, p.token_1_decimals) AS token_1_in
+    FROM
+        hourly_pools p
+    LEFT JOIN
+        mint_agg m ON p.hour = m.hour AND p.pool_id = m.poolId
+    LEFT JOIN
+        burn_agg b ON p.hour = b.hour AND p.pool_id = b.poolId
+    LEFT JOIN
+        swap_agg s ON p.hour = s.hour AND p.pool_id = s.poolId
+    ORDER BY
+        p.hour, p.pool_id
 )
 
 SELECT
     p.hour AS hour,
     p.pool_id AS pool_id,
     SUM(p.net_minted) OVER (PARTITION BY p.pool_id ORDER BY hour) AS lp_supply,
-    SUM(p.token_0_inflow) OVER (PARTITION BY p.pool_id ORDER BY hour) AS token_0_balance,
-    SUM(p.token_1_inflow) OVER (PARTITION BY p.pool_id ORDER BY hour) AS token_1_balance
+    SUM(p.token_0_in) OVER (PARTITION BY p.pool_id ORDER BY hour) AS token_0_reserves,
+    SUM(p.token_1_in) OVER (PARTITION BY p.pool_id ORDER BY hour) AS token_1_reserves
 FROM
     hourly_pool_aggregated p
 ORDER BY
