dc6c5515204e3eedbbe019c3ddc474f504ee9340
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-01-10 16:20:11 -0500

    Mattias changes

--- src/processor.ts
+++ src/processor.ts
@@ -1,12 +1,31 @@
-import { FuelGlobalProcessor } from '@sentio/sdk/fuel';
-import { InputType, OutputType, Interface, bn, ReceiptType, ZeroBytes32 } from 'fuels';
-import { AmmProcessor } from './types/fuel/AmmProcessor.js'
-import { AMM_CONTRACT_ADDRESS, BASE_ASSET_ID, NETWORK_ID, NETWORK_NAME } from './const.js';
-import { getLPAssetId, normalizeTxDate, poolIdToStr } from './utils.js';
-import { SetDecimalsEventInput, SetNameEventInput, SetSymbolEventInput, Src20, Src20Interface } from './types/fuel/Src20.js';
-import verifiedAssets from './verified-assets.json' assert { type: 'json' };
-import { Pool } from './schema/store.js';
-import { getPoolSnapshot, newPool } from './entities.js';
+import { FuelContractContext, FuelGlobalProcessor } from "@sentio/sdk/fuel";
+import {
+  InputType,
+  OutputType,
+  Interface,
+  bn,
+  ReceiptType,
+  ZeroBytes32,
+} from "fuels";
+import { AmmProcessor } from "./types/fuel/AmmProcessor.js";
+import {
+  AMM_CONTRACT_ADDRESS,
+  BASE_ASSET_ID,
+  NETWORK_ID,
+  NETWORK_NAME,
+} from "./const.js";
+import { getLPAssetId, normalizeTxDate, PoolId, poolIdToStr } from "./utils.js";
+import {
+  SetDecimalsEventInput,
+  SetNameEventInput,
+  SetSymbolEventInput,
+  Src20,
+  Src20Interface,
+} from "./types/fuel/Src20.js";
+import verifiedAssets from "./verified-assets.json";
+import { Pool } from "./schema/store.js";
+import { getPoolSnapshot, newPool } from "./entities.js";
+import { Amm } from "./types/fuel/Amm.js";
 
 const setNameEventId = "7845998088195677205";
 const setSymbolEventId = "12152039456660331088";
@@ -14,9 +33,24 @@ const setDecimalsEventId = "18149631459970394923";
 
 const src20Interface = new Interface(Src20.abi);
 
-const ETH_ASSET_ID = '0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07';
+const ETH_ASSET_ID =
+  "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07";
 
 // const sha256 = (str: string) => crypto.createHash('sha256').update(str).digest('hex');
+
+const getOrCreatePool = async (
+  poolId: PoolId,
+  ctx: FuelContractContext<Amm>
+) => {
+  let pool: Pool;
+  try {
+    pool = (await ctx.store.get(Pool, poolIdToStr(poolId)))!;
+  } catch (error) {
+    await newPool(poolId, ctx);
+    pool = (await ctx.store.get(Pool, poolIdToStr(poolId)))!;
+  }
+  return pool;
+};
 
 const processor = AmmProcessor.bind({
   address: AMM_CONTRACT_ADDRESS,
@@ -24,8 +58,8 @@ const processor = AmmProcessor.bind({
 });
 
 processor.onLogCreatePoolEvent(async (event, ctx) => {
-  if (ctx.transaction?.status === 'success') {
-    ctx.meter.Counter('pools').add(1);
+  if (ctx.transaction?.status === "success") {
+    ctx.meter.Counter("pools").add(1);
     ctx.eventLogger.emit("PairCreated", {
       poolId: poolIdToStr(event.data.pool_id),
       token0: event.data.pool_id[0].bits,
@@ -34,50 +68,68 @@ processor.onLogCreatePoolEvent(async (event, ctx) => {
       lpAssetId: getLPAssetId(event.data.pool_id),
     });
 
-    await newPool(event.data.pool_id, ctx);
+    await getOrCreatePool(event.data.pool_id, ctx);
   }
 });
 
 processor.onLogSwapEvent(async (event, ctx) => {
-  if (ctx.transaction?.status === 'success') {
+  if (ctx.transaction?.status === "success") {
     ctx.eventLogger.emit("Swap", {
       poolId: poolIdToStr(event.data.pool_id),
       token0In: event.data.asset_0_in,
       token1In: event.data.asset_1_in,
       token0Out: event.data.asset_0_out,
       token1Out: event.data.asset_1_out,
-      recipient: event.data.recipient.Address?.bits || event.data.recipient.ContractId?.bits,
+      recipient:
+        event.data.recipient.Address?.bits ||
+        event.data.recipient.ContractId?.bits,
     });
 
-    const pool = (await ctx.store.get(Pool, poolIdToStr(event.data.pool_id)))!;
-    pool.reserve0 = pool.reserve0 + BigInt(event.data.asset_0_in.toString()) - BigInt(event.data.asset_0_out.toString());
-    pool.reserve1 += BigInt(event.data.asset_1_in.toString()) - BigInt(event.data.asset_1_out.toString());
-    pool.volumeAsset0 += BigInt(event.data.asset_0_in.toString()) + BigInt(event.data.asset_0_out.toString());
-    pool.volumeAsset1 += BigInt(event.data.asset_1_in.toString()) + BigInt(event.data.asset_1_out.toString());
+    const pool = await getOrCreatePool(event.data.pool_id, ctx);
+
+    pool.reserve0 =
+      pool.reserve0 +
+      BigInt(event.data.asset_0_in.toString()) -
+      BigInt(event.data.asset_0_out.toString());
+    pool.reserve1 +=
+      BigInt(event.data.asset_1_in.toString()) -
+      BigInt(event.data.asset_1_out.toString());
+    pool.volumeAsset0 +=
+      BigInt(event.data.asset_0_in.toString()) +
+      BigInt(event.data.asset_0_out.toString());
+    pool.volumeAsset1 +=
+      BigInt(event.data.asset_1_in.toString()) +
+      BigInt(event.data.asset_1_out.toString());
 
     const snapshot = await getPoolSnapshot(pool, ctx.timestamp, ctx);
     snapshot.transactions += 1;
     snapshot.reserve0 = pool.reserve0;
     snapshot.reserve1 = pool.reserve1;
-    snapshot.volumeAsset0 += BigInt(event.data.asset_0_in.toString()) + BigInt(event.data.asset_0_out.toString());
-    snapshot.volumeAsset1 += BigInt(event.data.asset_1_in.toString()) + BigInt(event.data.asset_1_out.toString());
+    snapshot.volumeAsset0 +=
+      BigInt(event.data.asset_0_in.toString()) +
+      BigInt(event.data.asset_0_out.toString());
+    snapshot.volumeAsset1 +=
+      BigInt(event.data.asset_1_in.toString()) +
+      BigInt(event.data.asset_1_out.toString());
     await ctx.store.upsert(snapshot);
     await ctx.store.upsert(pool);
   }
 });
 
 processor.onLogMintEvent(async (event, ctx) => {
-  if (ctx.transaction?.status === 'success') {
+  if (ctx.transaction?.status === "success") {
     ctx.eventLogger.emit("Mint", {
       poolId: poolIdToStr(event.data.pool_id),
       token0In: event.data.asset_0_in,
       token1In: event.data.asset_1_in,
       liquidity: event.data.liquidity.amount,
-      recipient: event.data.recipient.Address?.bits || event.data.recipient.ContractId?.bits,
+      recipient:
+        event.data.recipient.Address?.bits ||
+        event.data.recipient.ContractId?.bits,
       lpAssetId: event.data.liquidity.id.bits,
     });
 
-    const pool = (await ctx.store.get(Pool, poolIdToStr(event.data.pool_id)))!;
+    const pool = await getOrCreatePool(event.data.pool_id, ctx);
     pool.reserve0 += BigInt(event.data.asset_0_in.toString());
     pool.reserve1 += BigInt(event.data.asset_1_in.toString());
     pool.lpTokenSupply += BigInt(event.data.liquidity.amount.toString());
@@ -93,17 +145,19 @@ processor.onLogMintEvent(async (event, ctx) => {
 });
 
 processor.onLogBurnEvent(async (event, ctx) => {
-  if (ctx.transaction?.status === 'success') {
+  if (ctx.transaction?.status === "success") {
     ctx.eventLogger.emit("Burn", {
       poolId: poolIdToStr(event.data.pool_id),
       token0Out: event.data.asset_0_out,
       token1Out: event.data.asset_1_out,
       liquidity: event.data.liquidity.amount,
-      recipient: event.data.recipient.Address?.bits || event.data.recipient.ContractId?.bits,
+      recipient:
+        event.data.recipient.Address?.bits ||
+        event.data.recipient.ContractId?.bits,
       lpAssetId: event.data.liquidity.id.bits,
     });
 
-    const pool = (await ctx.store.get(Pool, poolIdToStr(event.data.pool_id)))!;
+    const pool = await getOrCreatePool(event.data.pool_id, ctx);
     pool.reserve0 -= BigInt(event.data.asset_0_out.toString());
     pool.reserve1 -= BigInt(event.data.asset_1_out.toString());
     pool.lpTokenSupply -= BigInt(event.data.liquidity.amount.toString());
@@ -118,82 +172,92 @@ processor.onLogBurnEvent(async (event, ctx) => {
   }
 });
 
-processor.onTimeInterval(async (block, ctx) => {
-  const poolGen = ctx.store.listIterator(Pool, []);
-  for await (const pool of poolGen) {
-    const snapshot = await getPoolSnapshot(pool, ctx.timestamp, ctx);
-    await ctx.store.upsert(snapshot);
+processor.onTimeInterval(
+  async (block, ctx) => {
+    const poolGen = ctx.store.listIterator(Pool, []);
+    for await (const pool of poolGen) {
+      const snapshot = await getPoolSnapshot(pool, ctx.timestamp, ctx);
+      await ctx.store.upsert(snapshot);
+    }
+  },
+  60,
+  60
+);
+
+FuelGlobalProcessor.bind({
+  chainId: NETWORK_ID,
+  startBlock: 3439718n,
+}).onTransaction(async (tx, ctx) => {
+  if (tx.blockNumber === "1") {
+    ctx.eventLogger.emit("SetName", {
+      assetId: ETH_ASSET_ID,
+      name: "Ether",
+    });
+    ctx.eventLogger.emit("SetSymbol", {
+      assetId: ETH_ASSET_ID,
+      symbol: "Symbol",
+    });
+    ctx.eventLogger.emit("SetDecimals", {
+      assetId: ETH_ASSET_ID,
+      decimals: 9,
+    });
+
+    for (const asset of verifiedAssets) {
+      const assetChain = asset.networks.find(
+        (n) => n.chain === NETWORK_NAME && n.type === "fuel"
+      );
+      if (assetChain) {
+        ctx.eventLogger.emit("VerifiedAsset", {
+          assetId: assetChain.assetId,
+          name: asset.name,
+          symbol: asset.symbol,
+          decimals: assetChain.decimals,
+        });
+      }
+    }
   }
-}, 60, 60);
 
-FuelGlobalProcessor
-  .bind({ chainId: NETWORK_ID })
-  .onTransaction(
-    async (tx, ctx) => {
-      if (tx.blockNumber === '1') {
-        ctx.eventLogger.emit('SetName', {
-          assetId: ETH_ASSET_ID,
-          name: 'Ether',
-        });
-        ctx.eventLogger.emit('SetSymbol', {
-          assetId: ETH_ASSET_ID,
-          symbol: 'Symbol',
-        });
-        ctx.eventLogger.emit('SetDecimals', {
-          assetId: ETH_ASSET_ID,
-          decimals: 9,
-        });
+  const txDate = tx.date ? normalizeTxDate(tx.date) : null;
 
-        for (const asset of verifiedAssets) {
-          const assetChain = asset.networks.find((n) => n.chain === NETWORK_NAME && n.type === 'fuel');
-          if (assetChain) {
-            ctx.eventLogger.emit('VerifiedAsset', {
-              assetId: assetChain.assetId,
-              name: asset.name,
-              symbol: asset.symbol,
-              decimals: assetChain.decimals,
+  if (tx.status === "success") {
+    for (const receipt of tx.receipts) {
+      if (receipt.type === ReceiptType.Mint) {
+        ctx.eventLogger.emit("AssetMint", {
+          assetId: receipt.assetId,
+          contractId: receipt.contractId,
+          subId: receipt.subId,
+        });
+      } else if (receipt.type === ReceiptType.LogData) {
+        switch (receipt.val1.toString()) {
+          case setNameEventId:
+            const [nameEvent]: [SetNameEventInput] = src20Interface.decodeLog(
+              receipt.data,
+              setNameEventId
+            );
+
+            ctx.eventLogger.emit("SetName", {
+              assetId: nameEvent.asset.bits,
+              name: nameEvent.name,
             });
-          }
+            break;
+          case setSymbolEventId:
+            const [symbolEvent]: [SetSymbolEventInput] =
+              src20Interface.decodeLog(receipt.data, setSymbolEventId);
+            ctx.eventLogger.emit("SetSymbol", {
+              assetId: symbolEvent.asset.bits,
+              symbol: symbolEvent.symbol,
+            });
+            break;
+          case setDecimalsEventId:
+            const [decimalsEvent]: [SetDecimalsEventInput] =
+              src20Interface.decodeLog(receipt.data, setDecimalsEventId);
+            ctx.eventLogger.emit("SetDecimals", {
+              assetId: decimalsEvent.asset.bits,
+              decimals: decimalsEvent.decimals,
+            });
+            break;
         }
       }
-
-      const txDate = tx.date ? normalizeTxDate(tx.date) : null;
-
-      if (tx.status === 'success') {
-        for (const receipt of tx.receipts) {
-          if (receipt.type === ReceiptType.Mint) {
-            ctx.eventLogger.emit('AssetMint', {
-              assetId: receipt.assetId,
-              contractId: receipt.contractId,
-              subId: receipt.subId,
-            });
-          } else if (receipt.type === ReceiptType.LogData) {
-            switch (receipt.val1.toString()) {
-              case setNameEventId:
-                const [nameEvent]: [SetNameEventInput] = src20Interface.decodeLog(receipt.data, setNameEventId);
-
-                ctx.eventLogger.emit('SetName', {
-                  assetId: nameEvent.asset.bits,
-                  name: nameEvent.name,
-                });
-                break;
-              case setSymbolEventId:
-                const [symbolEvent]: [SetSymbolEventInput] = src20Interface.decodeLog(receipt.data, setSymbolEventId);
-                ctx.eventLogger.emit('SetSymbol', {
-                  assetId: symbolEvent.asset. bits,
-                  symbol: symbolEvent.symbol,
-                });
-                break;
-              case setDecimalsEventId:
-                const [decimalsEvent]: [SetDecimalsEventInput] = src20Interface.decodeLog(receipt.data, setDecimalsEventId);
-                ctx.eventLogger.emit('SetDecimals', {
-                  assetId: decimalsEvent.asset.bits,
-                  decimals: decimalsEvent.decimals,
-                });
-                break;
-            }
-          }
-        }
-      }
-    },
-  );
+    }
+  }
+});
