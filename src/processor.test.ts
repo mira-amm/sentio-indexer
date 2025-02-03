f1bed780de2263fe5772996a88d4ed6b18bb2164
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-02-03 13:25:03 -0500

    Rework test framework to accept a directory of JSON files

--- src/processor.test.ts
+++ src/processor.test.ts
@@ -1,14 +1,22 @@
 import assert from 'assert'
 import { before, describe, test } from 'node:test'
 import { TestProcessorServer } from '@sentio/sdk/testing'
-import {
-  AMM_CONTRACT_ADDRESS,
-  BASE_ASSET_ID,
-  NETWORK_ID,
-  NETWORK_NAME,
-} from "./const.js";
-import testData from './test-data.json' assert {type: 'json'};
+import {NETWORK_ID} from "./const.js";
+import fs from 'fs';
 import { PoolSnapshot, Pool } from "./schema/store.js";
+
+const test_data: JSON[] = [];
+const test_data_dir = "./src/test-data";
+fs.readdirSync(test_data_dir).forEach((file) => {
+  fs.readFile(`${test_data_dir}/${file}`, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+    test_data.push(JSON.parse(data));
+  });
+});
+
 
 describe('Test Processor', () => {
   const service = new TestProcessorServer(() => import('./processor.js'))
@@ -21,16 +29,16 @@ describe('Test Processor', () => {
     const config = await service.getConfig({})
     assert(config.contractConfigs.length > 0)
     // CreatePoolEvent, SwapEvent, MintEvent, BurnEvent
-    assert.equal(config.contractConfigs[0].fuelLogConfigs.length, 4)
+    assert.equal(config.contractConfigs[0].fuelReceiptConfigs.length, 4)
   })
   
-
   test('test onLog ', async () => {
-    const res = await service.fuel.testOnTransaction(testData, NETWORK_ID);
-    // console.log(JSON.stringify(res))
-    console.log((await service.store.list(Pool))[0])
-    console.log(await service.store.list(PoolSnapshot))
-
+    for (const testData of test_data) {
+      const res = await service.fuel.testOnTransaction(testData, NETWORK_ID);
+      // console.log(JSON.stringify(res))
+    }
+    assert.equal((await service.store.list(Pool)).length, 26)
+    assert.equal((await service.store.list(PoolSnapshot)).length, 26)
   });
 
 })
